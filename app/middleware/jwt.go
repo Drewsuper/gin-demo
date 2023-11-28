4a72a356159276664170740b2cf44fb0618669c5
LiHan <LF20182003@outlook.com>
2023-11-28 20:54:28 +0800

    update file directory structure

--- app/middleware/jwt.go
+++ app/middleware/jwt.go
@@ -1,34 +1,10 @@
 package middleware
 
 import (
-	"errors"
-	"gin-new/app/config"
 	"gin-new/app/types"
+	"gin-new/app/utils"
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
-	"time"
 )
-
-type MyClaims struct {
-	Data any `json:"data"`
-	jwt.RegisteredClaims
-}
-
-func ParseToken(token string) (*MyClaims, error) {
-	mySecret := []byte(config.Config.JWT.Secret)
-	jwtToken, err := jwt.ParseWithClaims(token, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return mySecret, nil
-	})
-	if err != nil {
-		return nil, err
-	} else {
-		claims, errOne := jwtToken.Claims.(*MyClaims)
-		if errOne && jwtToken.Valid {
-			return claims, nil
-		}
-		return nil, errors.New("token无法解析")
-	}
-}
 
 func Auth() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
@@ -41,7 +17,7 @@ func Auth() gin.HandlerFunc {
 			})
 			return
 		}
-		_, err := ParseToken(auth)
+		_, err := utils.ParseToken(auth)
 		if err != nil {
 			ctx.Abort()
 			ctx.JSON(400, types.CommonRps{
@@ -52,21 +28,4 @@ func Auth() gin.HandlerFunc {
 		}
 		ctx.Next()
 	}
-}
-
-func NewToken(data any) (string, error) {
-	claims := MyClaims{
-		data,
-		jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(5 * time.Hour)),
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-			NotBefore: jwt.NewNumericDate(time.Now()),
-		},
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	s, err := token.SignedString([]byte(config.Config.JWT.Secret))
-	if err != nil {
-		return "", err
-	}
-	return s, nil
 }
